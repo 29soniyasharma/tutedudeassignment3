250cef610570ba6a99c58dad452f8cd7dabd68af
29soniyasharma <29soniyasharma@gmail.com>
2025-10-22 12:36:11 +0530

    Using the Math Module for Calculations

---
+++ Task2.py
@@ -0,0 +1,8 @@
+import math as mt
+num=float(input("Enter a number: "))
+square_root=mt.sqrt(num)
+print("Square root :", square_root)
+log=mt.log(num)
+print("Logarithm:",log)
+sine=mt.sin(num)
+print("Sine :",sine)
